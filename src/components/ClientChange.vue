ee24d6bdacfccffa8fdf1735a39e99a22bad406f
beisen <luochongjie@beisen.com>
2018-04-30 14:04:34 +0800

    变更管理

--- src/components/ClientChange.vue
+++ src/components/ClientChange.vue
@@ -1,3 +1,213 @@
 <template>
-  <div>变更管理</div>
+  <div>
+    <el-table
+      ref="multipleTable"
+      :data="tableData3"
+      tooltip-effect="dark"
+      style="width: 100%"
+      @selection-change="handleSelectionChange">
+      <el-table-column
+        type="selection"
+        width="55">
+      </el-table-column>
+      <el-table-column
+        label="日期"
+        width="120">
+        <template slot-scope="scope">{{ scope.row.date }}</template>
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+        prop="tag"
+        label="状态"
+      >
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.tag === '报停' ? 'primary' : 'success'"
+            close-transition>{{scope.row.tag}}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="120">
+        <template slot-scope="scope">
+          <el-button
+            @click.native.prevent="SendMsg(scope.$index, scope.row.tag)"
+            type="text">
+            用热状态变更
+          </el-button>
+          <el-button type="text" @click="open5">客户类型变更</el-button>
+          <el-button type="text" @click="open4">计费变更</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div style="margin-top: 20px">
+      <el-button @click="toggleSelection(tableData3)">批量发送催缴短信</el-button>
+    </div>
+  </div>
+
 </template>
+
+<script>
+  import Axios from 'axios';
+
+  export default {
+    created() {
+      Axios.post('http://127.0.0.1:80/heatphp/welcome/arrears_information').then((res) => {
+        if (res) {
+          var arr = this.tableData3;
+          var data = res.data;
+          arr.length = data.length;
+          for (var i = 0; i < data.length; i++) {
+            arr[i].date = data[i].user_entertime;
+            arr[i].name = data[i].user_name;
+            arr[i].address = data[i].user_address;
+            arr[i].tag = '';
+          }
+          this.tableData3 = arr;
+        }
+
+      });
+    },
+    data() {
+      return {
+        tableData3: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-08',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-06',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }, {
+          date: '2016-05-07',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '正常'
+        }],
+        multipleSelection: [],
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value4: []
+      }
+    },
+
+    methods: {
+      open5() {
+        this.$alert(`
+  <el-select v-model="value4" clearable placeholder="请选择">
+      <el-option
+      v-for="item in this.options"
+      :key="item.value"
+      :label="item.label"
+      :value="item.value">
+       </el-option>
+  </el-select>
+`, 'HTML 片段', {
+          dangerouslyUseHTMLString: true
+        });
+      },
+      open4() {
+        const h = this.$createElement;
+        this.$msgbox({
+          title: '消息',
+          message: h('p', null, [
+            h('span', null, '计费方式 '),
+            h('i', {style: 'color: teal'}, '面积供热')
+          ]),
+          showCancelButton: true,
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          beforeClose: (action, instance, done) => {
+            if (action === 'confirm') {
+              instance.confirmButtonLoading = true;
+              instance.confirmButtonText = '执行中...';
+              setTimeout(() => {
+                done();
+                instance.confirmButtonLoading = false;
+              }, 0);
+            } else {
+              done();
+            }
+          }
+        }).then(() => {
+          this.$message({
+            type: 'success',
+            message: '变更成功!'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '取消变更'
+          });
+        });
+        ;
+
+      },
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row, true);
+            row.tag = '已批量发送催缴短信'
+          });
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      },
+      SendMsg(index, rows) {
+        rows = '报停';
+        this.tableData3[index].tag = rows
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      },
+    }
+  }
+</script>
